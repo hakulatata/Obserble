a68689b1dabbb29faee673a617a21ffe0ef46fac
zyh <hakulatata@163.com>
2017-04-22 10:05:26 +0800

    ssssssssssssssssss

---
+++ Obserblexx/app/src/main/java/hakulatata/com/o/Hakulatata.java
@@ -0,0 +1,23 @@
+package hakulatata.com.o;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/*
+ *  @项目名：  Obserblexx 
+ *  @包名：    hakulatata.com.o
+ *  @文件名:   Hakulatata
+ *  @创建者:   lenovo
+ *  @创建时间:  2017/4/22 9:58
+ *  @描述：    TODO
+ */
+public class Hakulatata extends Activity{
+    private static final String TAG = "Hakulatata";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
---
+++ Obserblexx/app/src/main/java/hakulatata/com/o/asdfa.java
@@ -0,0 +1,13 @@
+package hakulatata.com.o;
+
+/*
+ *  @项目名：  Obserblexx 
+ *  @包名：    hakulatata.com.o
+ *  @文件名:   asdfa
+ *  @创建者:   lenovo
+ *  @创建时间:  2017/4/22 10:05
+ *  @描述：    TODO
+ */
+public class asdfa {
+    private static final String TAG = "asdfa";
+}
